c7e83d471b5cbdca4b7c3180742b9165de5bf9f1
csheehp <chiranjit.shee@hpe.com>
2025-03-21 20:20:30 +0530

    monolith attempt

---
+++ console/components/signin/signinEmailPrompt.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="q-pa-md fit row justify-center q-gutter-md;">
+    <q-card class="q-pa-md; q-gutter-md; q-ma-md; col-8">
+      <q-card-section>
+        <div class="text-h6 q-py-md">Welcome to Warg Studio</div>
+        <div class="text-h7 q-py-md">Enter your login information</div>
+      </q-card-section>
+      <q-card-section>
+        <q-input
+          color="grey-3"
+          label-color="orange"
+          class="q-w-50 shadow-2"
+          outlined
+          v-model="mail"
+          label="Email"
+        >
+          <template v-slot:append>
+            <q-icon name="mail" color="orange" />
+          </template>
+        </q-input>
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn
+          no-caps
+          outline
+          padding="xs lg"
+          color="grey"
+          @click="emitterRegister"
+          >Register</q-btn
+        >
+        <q-btn no-caps :disable="isDisable" padding="xs lg" color="primary"
+          >Next</q-btn
+        >
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+<script lang="ts" setup>
+  import { ref, watch, defineEmits} from "vue";
+  const mail = ref("");
+  const isDisable = ref(true);
+
+  watch(mail, (newVal) => {
+    isDisable.value = !(newVal.length > 1 && newVal.includes("@"));
+  });
+  const emit = defineEmits(["register"]);
+
+  function emitterRegister() {
+    console.log("emitterRegister");
+    emit("register", true);
+  }
+</script>
+<style></style>
--- console/nuxt.config.ts
+++ console/nuxt.config.ts
@@ -4,7 +4,7 @@ import materialIconsRound from 'quasar/icon-set/svg-material-icons-round'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  ssr: true,
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
