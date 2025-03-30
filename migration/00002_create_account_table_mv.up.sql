e51198ec92eab31b6cbaae284224c068a66a611e
csheehp <chiranjit.shee@hpe.com>
2025-03-31 02:47:21 +0530

    31/03: account materialize view table

--- migration/00002_create_account_table_mv.up.sql
+++ migration/00002_create_account_table_mv.up.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 -- Create a materialized view for account events
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS account AS (
+CREATE MATERIALIZED VIEW IF NOT EXISTS account AS
 SELECT DISTINCT ON (event_data ->> 'account_name')
     event_data ->> 'account_name' AS account_name,
     SPLIT_PART(event_type,'_',2) AS status,
@@ -10,8 +10,7 @@ SELECT DISTINCT ON (event_data ->> 'account_name')
     created_at AS updated_at
 FROM events
 WHERE event_type ~~ 'account%'
-ORDER BY event_data ->> 'account_name', created_at DESC
-);
+ORDER BY event_data ->> 'account_name', created_at DESC;
 
 -- Create a index for querying events by stream
 
